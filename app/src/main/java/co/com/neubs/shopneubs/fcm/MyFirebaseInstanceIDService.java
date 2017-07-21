a4cb4119542b0cf53109853876e21c9a8a0e3c79
bikerlfh <bikerlfh@hotmail.com>
2017-07-21 01:20:01 -0500

    API

--- app/src/main/java/co/com/neubs/shopneubs/fcm/MyFirebaseInstanceIDService.java
+++ app/src/main/java/co/com/neubs/shopneubs/fcm/MyFirebaseInstanceIDService.java
@@ -5,14 +5,8 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import co.com.neubs.shopneubs.classes.APIRest;
-import co.com.neubs.shopneubs.classes.APIValidations;
 import co.com.neubs.shopneubs.classes.SessionManager;
 import co.com.neubs.shopneubs.classes.models.FireBaseToken;
-import co.com.neubs.shopneubs.interfaces.IServerCallback;
 
 /**
  * Created by bikerlfh on 7/19/17.
@@ -31,25 +25,20 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     // Guarda el token en la DB
     public void saveToken(String token){
+        // Se registra el token en el servidor
+        SessionManager.getInstance(getApplicationContext()).registerFCMToken(token);
+
         FireBaseToken fireBaseToken = new FireBaseToken();
         // se verifica si existe un registro
         if (fireBaseToken.exists()){
             fireBaseToken.setToken(token);
-            if(!fireBaseToken.update()){
+            if(!fireBaseToken.update())
                 Log.d(TAG,"Error al guardar el token fcm");
-                return;
-            }
         }
         else{
             fireBaseToken.setToken(token);
-            if (!fireBaseToken.save()){
+            if (!fireBaseToken.save())
                 Log.d(TAG,"Error al modificar el token fcm");
-                return;
-            }
         }
-
-        SessionManager sessionManager = SessionManager.getInstance(getApplicationContext());
-
-        sessionManager.registerFCMToken(token);
     }
 }
